4da163613adb79a7c7f7b67c49a40dcf16c80574
LittleMercyEh <liam@portent.ca>
2025-03-07 12:20:56 -0700

    exercise 3 added

---
+++ Ex3/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function About(props) {
+    return (
+    <div>
+        <h1>{props.title}</h1>
+        <p>{props.title}</p>
+    </div>
+    );
+}
+
+
+export default About;
