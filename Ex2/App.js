1f984c86ce4a18b791ffd193add95059b6ce067c
LittleMercyEh <liam@portent.ca>
2025-03-07 12:09:54 -0700

    exercise 2 uploaded

---
+++ Ex2/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact';
+
+function App() {
+  const currentYear = new Date().getFullYear();
+  const isLoggedIn = true;
+  return (
+    <div>
+      <p>React Components</p>
+      <p>Current Year: {currentYear}</p>
+      {isLoggedIn ? <p>Welcome back!</p> : <p>Please log in.</p>}
+      <Home title="Home Page" description="Welcome to Our website"/>
+      <About title="About Us" description="We are passionate about delivering quality experiences." />
+      <Contact title="Contact Us" description="Feel free to reach out to us via email or phone" />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
---
+++ Ex2/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Contact(props) {
+    return (
+    <div>
+        <h1>{props.title}</h1>
+        <p>{props.description}</p>
+    </div>
+    );
+
+}
+
+
+export default Contact;
